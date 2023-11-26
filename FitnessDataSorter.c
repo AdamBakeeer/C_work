44c47df36efccaeb68bbf32a6a850cefbe948481
adam bakeer <146668872+AdamBakeeer@users.noreply.github.com>
2023-11-26 03:15:42 +0000

    finall

---
+++ FitnessDataSorter.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define the struct for the fitness data
+typedef struct {
+    char date[11];
+    char time[6];
+    int steps;
+} FitnessData;
+
+int comparator(const void *x, const void *y) {
+    const FitnessData *a = (const FitnessData *)x;
+    const FitnessData *b = (const FitnessData *)y;
+    return b->steps - a->steps;
+}
+
+// Function to tokenize a record
+void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *steps) {
+    char *ptr = strtok(record, &delimiter);
+    if (ptr != NULL) {
+        strcpy(date, ptr);
+        ptr = strtok(NULL, &delimiter);
+        if (ptr != NULL) {
+            strcpy(time, ptr);
+            ptr = strtok(NULL, &delimiter);
+            if (ptr != NULL) {
+                *steps = atoi(ptr);
+            }
+        }
+    }
+}
+
+#define buffer_size 100
+#define max_record 100
+
+int main() {
+    char line[buffer_size];
+    char filename[buffer_size];
+    FILE *file = NULL;
+    int records = 0;
+    FitnessData fit[max_record];
+    
+    
+    printf("Enter filename:\n");
+    fgets(line, buffer_size, stdin);
+    sscanf(line, " %s ", filename);
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: Could not find or open the file.\n");
+        return 1;
+    }
+
+    while(fgets(line, buffer_size, file) != NULL){
+        tokeniseRecord(line, ',', fit[records].date, fit[records].time, &fit[records].steps);
+        
+        if (fit[records].steps <= 0) {
+            printf("Error: invalid file\n");
+        }
+
+        if (strlen(fit[records].date) == 0) {
+            printf("Error: invalid file\n");
+        }
+
+        if (strlen(fit[records].time) == 0) {
+            printf("Error: invalid file\n");
+        }
+        records++;
+    }
+    fclose(file);
+
+    qsort(fit, records, sizeof(FitnessData), comparator);
+
+    for (int i = 0; i < records; ++i) {
+        printf("%s\t%s\t%d\n", fit[i].date, fit[i].time, fit[i].steps);
+
+    char filename [] = "data.txt"
+    
+}
+return 0;
+}
+
